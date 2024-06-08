cb886903fba06fd36a79636424a970d5546ae6f2
ajohnson1031 <aaron.johnson1031@gmail.com>
2024-06-08 14:04:01 -0400

    restrict all wallets except Solflare

--- components/Wallets/Wallets.tsx
+++ components/Wallets/Wallets.tsx
@@ -7,24 +7,35 @@ const Wallets = ({ select, wallets, publicKey, disconnect }: WalletsProps) => {
 
   return !publicKey ? (
     <div className="flex gap-4 flex-col w-72">
-      {wallets.filter((wallet: any) => wallet.readyState === "Installed").length > 0 ? (
+      {wallets.filter((wallet: any) => wallet.readyState === "Installed" && wallet.adapter.name === "Solflare").length > 0 ? (
         wallets
-          .filter((wallet: any) => wallet.readyState === "Installed")
-          .map((wallet: any) => (
-            <button
-              className="w-full text-base flex gap-2 items-center justify-center bg-vulcan-300/60 py-2 rounded-sm hover:bg-vulcan-200/60 text-white"
-              key={wallet.adapter.name}
-              onClick={() => {
-                select(wallet.adapter.name);
-                setIsViewingWallet(false);
-              }}
-            >
-              <Img src={wallet.adapter.icon} alt={wallet.adapter.name} />
-              {wallet.adapter.name}
-            </button>
-          ))
+          .filter((wallet: any) => wallet.readyState === "Installed" && wallet.adapter.name === "Solflare")
+          .map((wallet: any) => {
+            console.log(wallet.adapter.name);
+            return (
+              <button
+                className="w-full text-base flex gap-2 items-center justify-center bg-vulcan-300/60 py-2 rounded-sm hover:bg-vulcan-200/60 text-white"
+                key={wallet.adapter.name}
+                onClick={() => {
+                  select(wallet.adapter.name);
+                  setIsViewingWallet(false);
+                }}
+              >
+                <Img src={wallet.adapter.icon} alt={wallet.adapter.name} />
+                {wallet.adapter.name}
+              </button>
+            );
+          })
       ) : (
-        <p className="text-gray-300">No wallet found. Please download a supported Solana wallet</p>
+        <div className="text-gray-300">
+          <span className="text-red-300">No supported wallet found.</span>
+          <br />
+          Please download the&nbsp;
+          <a href="https://solflare.com/" target="_blank" rel="noopener noreferrer" className="text-blue-400 underline">
+            Solflare Wallet
+          </a>{" "}
+          extension for your browser.
+        </div>
       )}
     </div>
   ) : (
