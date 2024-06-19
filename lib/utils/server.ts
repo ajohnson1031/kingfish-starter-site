2a665f806290fa4ed6ec2cbf8ed37089916c8a7b
ajohnson1031 <aaron.johnson1031@gmail.com>
2024-06-19 13:48:02 -0400

    remove mock api call

--- lib/utils/server.ts
+++ lib/utils/server.ts
@@ -69,8 +69,7 @@ const getUnprivilegedUserBalance = async (publicKey: string) => {
 const breakFishbowl = async (publicKey: string, tokenAmt: number, txid: string, walletName: string, email: string, paymentOption: string, pythPrice: any) => {
   try {
     const walletEmail = email.length > 0 ? email : null;
-    // const response = await fetch(`${process.env.NEXT_PUBLIC_MICROSERVICE_URL}/presale/break-fishbowl`, {
-    const response = await fetch(`http://localhost:5010/presale/break-fishbowl`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_MICROSERVICE_URL}/presale/break-fishbowl`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
