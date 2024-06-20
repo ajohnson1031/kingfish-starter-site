37f8659cfbaa3a7ca1b09c3261a942e1da711930
ajohnson1031 <aaron.johnson1031@gmail.com>
2024-06-20 15:21:46 -0400

    update wallet filter for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,7 @@ import { ComingSoon, Community, Ecosystem, Footer, HeroCard, Navbar, Roadmap, So
 import PresaleWindow from "@/components/PresaleWindow";
 import { getCurrentPresaleStageDetails } from "@/lib/utils/server";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
-import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import {
-  CoinbaseWalletAdapter,
-  LedgerWalletAdapter,
-  PhantomWalletAdapter,
-  SolflareWalletAdapter,
-  TorusWalletAdapter,
-  TrezorWalletAdapter,
-  TrustWalletAdapter,
-} from "@solana/wallet-adapter-wallets";
+import { LedgerWalletAdapter, TorusWalletAdapter, TrezorWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { clusterApiUrl } from "@solana/web3.js";
 import Head from "next/head";
 import { useEffect, useMemo } from "react";
@@ -20,18 +11,7 @@ import { COMMUNITY_CARDS, ECO_CARDS, ROADMAP } from "./constants";
 import { useViewerContext } from "./context/ViewerContext";
 
 export default function Home() {
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SolflareWalletAdapter(),
-      new CoinbaseWalletAdapter(),
-      new LedgerWalletAdapter(),
-      new TorusWalletAdapter(),
-      new TrezorWalletAdapter(),
-      new TrustWalletAdapter(),
-    ],
-    []
-  );
+  const wallets = useMemo(() => [new LedgerWalletAdapter(), new TorusWalletAdapter(), new TrezorWalletAdapter()], []);
 
   const endpoint = useMemo(() => clusterApiUrl("mainnet-beta"), []);
   const { isViewingPresale, isViewingComingSoon, setIsViewingComingSoon, setCurrentStageDetails } = useViewerContext();
@@ -45,60 +25,54 @@ export default function Home() {
   return (
     <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <Head>
-            <title>KingFish - The Breakout Meme Coin of 2024</title>
-            <meta
-              name="description"
-              content="Discover KingFish, the breakout meme coin of 2024. Join our decentralized community and explore unique features, robust technology on the Solana blockchain, and exciting growth opportunities."
-            />
-            <meta
-              name="keywords"
-              content="KingFish, KFSH, meme coin, cryptocurrency, Solana blockchain, decentralized finance, crypto community, staking rewards, token exchange"
-            />
-            <meta name="author" content="KingFish Team" />
+        <Head>
+          <title>KingFish - The Breakout Meme Coin of 2024</title>
+          <meta
+            name="description"
+            content="Discover KingFish, the breakout meme coin of 2024. Join our decentralized community and explore unique features, robust technology on the Solana blockchain, and exciting growth opportunities."
+          />
+          <meta name="keywords" content="KingFish, KFSH, meme coin, cryptocurrency, Solana blockchain, decentralized finance, crypto community, staking rewards, token exchange" />
+          <meta name="author" content="KingFish Team" />
 
-            <meta property="og:title" content="KingFish - The Breakout Meme Coin of 2024" />
-            <meta
-              property="og:description"
-              content="Join KingFish and be part of a revolutionary community-driven cryptocurrency. Explore our features, robust technology, and potential for significant growth."
-            />
-            <meta property="og:image" content="https://kingfish.app/kingfish-logo.png" />
-            <meta property="og:url" content="https://kingfish.app" />
+          <meta property="og:title" content="KingFish - The Breakout Meme Coin of 2024" />
+          <meta
+            property="og:description"
+            content="Join KingFish and be part of a revolutionary community-driven cryptocurrency. Explore our features, robust technology, and potential for significant growth."
+          />
+          <meta property="og:image" content="https://kingfish.app/kingfish-logo.png" />
+          <meta property="og:url" content="https://kingfish.app" />
 
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content="KingFish - The Breakout Meme Coin of 2024" />
-            <meta
-              name="twitter:description"
-              content="Discover KingFish, a revolutionary meme coin on the Solana blockchain. Join our community and explore unique features and growth opportunities."
-            />
-            <meta name="twitter:image" content="https://kingfish.app/kingfish-logo.png" />
-            <meta name="robots" content="index, follow" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-            <link rel="icon" href="/favicon.ico" />
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-            <link rel="manifest" href="/site.webmanifest" />
-            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-            <link rel="canonical" href="https://kingfish.app" />
-            <meta name="msapplication-TileColor" content="#da532c" />
-            <meta name="theme-color" content="#ffffff" />
-          </Head>
-          <main className="flex flex-col items-center px-5 lg:px-10 bg-vulcan-900 bg-no-repeat">
-            <Navbar />
-            <HeroCard />
-            <WalletMultiButton />
-            <Socials />
-            <Ecosystem ecocards={ECO_CARDS} />
-            <Tokenomics />
-            <Roadmap roadmapItems={ROADMAP} />
-            <Community communityItems={COMMUNITY_CARDS} />
-          </main>
-          {!isViewingPresale && !isViewingComingSoon && <Footer />}
-          {isViewingComingSoon && <ComingSoon isViewingComingSoon={isViewingComingSoon} setIsViewingComingSoon={setIsViewingComingSoon} />}
-          {isViewingPresale && <PresaleWindow />}
-        </WalletModalProvider>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="KingFish - The Breakout Meme Coin of 2024" />
+          <meta
+            name="twitter:description"
+            content="Discover KingFish, a revolutionary meme coin on the Solana blockchain. Join our community and explore unique features and growth opportunities."
+          />
+          <meta name="twitter:image" content="https://kingfish.app/kingfish-logo.png" />
+          <meta name="robots" content="index, follow" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <link rel="canonical" href="https://kingfish.app" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <main className="flex flex-col items-center px-5 lg:px-10 bg-vulcan-900 bg-no-repeat">
+          <Navbar />
+          <HeroCard />
+          <Socials />
+          <Ecosystem ecocards={ECO_CARDS} />
+          <Tokenomics />
+          <Roadmap roadmapItems={ROADMAP} />
+          <Community communityItems={COMMUNITY_CARDS} />
+        </main>
+        {!isViewingPresale && !isViewingComingSoon && <Footer />}
+        {isViewingComingSoon && <ComingSoon isViewingComingSoon={isViewingComingSoon} setIsViewingComingSoon={setIsViewingComingSoon} />}
+        {isViewingPresale && <PresaleWindow />}
       </WalletProvider>
     </ConnectionProvider>
   );
--- components/Wallets/Wallets.tsx
+++ components/Wallets/Wallets.tsx
@@ -5,26 +5,26 @@ import { WalletsProps } from "./Wallets.types";
 const Wallets = ({ select, wallets, publicKey, disconnect }: WalletsProps) => {
   const { setIsViewingWallet } = useViewerContext();
 
+  const availableWallets = wallets.filter((wallet: any) => wallet.readyState === "Installed" || wallet.readyState === "Loadable" || wallet.readyState === "NotDetected");
+
   return !publicKey ? (
     <div className="flex gap-4 flex-col w-72">
-      {wallets.filter((wallet: any) => wallet.readyState === "Installed").length > 0 ? (
-        wallets
-          .filter((wallet: any) => wallet.readyState === "Installed")
-          .map((wallet: any) => {
-            return (
-              <button
-                className="w-full text-base flex gap-2 items-center justify-center bg-vulcan-300/60 py-2 rounded-sm hover:bg-vulcan-200/60 text-white"
-                key={wallet.adapter.name}
-                onClick={() => {
-                  select(wallet.adapter.name);
-                  setIsViewingWallet(false);
-                }}
-              >
-                <Img src={wallet.adapter.icon} alt={wallet.adapter.name} />
-                {wallet.adapter.name}
-              </button>
-            );
-          })
+      {availableWallets.length > 0 ? (
+        availableWallets.map((wallet: any) => {
+          return (
+            <button
+              className="w-full text-base flex gap-2 items-center justify-center bg-vulcan-300/60 py-2 rounded-sm hover:bg-vulcan-200/60 text-white"
+              key={wallet.adapter.name}
+              onClick={() => {
+                select(wallet.adapter.name);
+                setIsViewingWallet(false);
+              }}
+            >
+              <Img src={wallet.adapter.icon} alt={wallet.adapter.name} />
+              {wallet.adapter.name}
+            </button>
+          );
+        })
       ) : (
         <div className="text-gray-300">
           <span className="text-red-300">No supported wallet found.</span>
